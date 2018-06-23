616bfb3e1c02c61e53c5dcc9781d93a23a3916d9
juancyc <juancyc@hotmail.com>
2018-06-22 23:42:15 -0300

    Correccion 1
    Estilo y codificacion UTF-8

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -3,7 +3,7 @@ package loom.hola_mundo;
 public class HolaMundo {
 
 	public String saludar(final String persona) {
-        // throw new RuntimeException("¡Implementar este método!");
-        return "¡Hola, "+persona+"!";
+        // throw new RuntimeException("Â¡Implementar este mÃ©todo!");
+        return "Â¡Hola, " + persona + "!" ;
     }
 }
